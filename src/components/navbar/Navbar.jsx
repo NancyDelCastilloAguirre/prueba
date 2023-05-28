d91232ea03c2dc3ebbfd47e6f39636b7a85a90d1
NancyDelCastilloAguirre <113216722+NancyDelCastilloAguirre@users.noreply.github.com>
2023-05-27 20:28:23 -0600

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,56 @@
 import "./navbar.css"
 import SONA from "../../images/SONA.png"
+import { Drawer, Space, Button} from 'antd';
+import { useState } from "react";
+
 export default function Navbar() {
+  
+  const [visible, setVisible] = useState(false);
+  const showDrawer = () => {
+    setVisible(true);
+  };
+  const onClose = () => {
+    setVisible(false);
+  };
+  
   return (
     <>
         <nav class="navbar navbar-expand-lg navbar-light foi-navbar">
             <a class="navbar-brand">
                 <img src={SONA} alt="SONA" className="sonaIcon" />
             </a>
-            <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <div>
+            <button onClick={showDrawer} class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
             </button>
+            <div>
+                <Drawer
+                    title= {`MENÚ`}
+                    className="draw"
+                    placement="right"
+                    onClose={onClose}
+                    visible={visible}
+                >
+                    <a class="navbar-brand">
+                        <img src={SONA} alt="SONA" className="sonaIcon" />
+                    </a>
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                            <li class="nav-item active">
+                                <a class="nav-link">Inicio</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link">Nosotros</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link">Caracteristicas</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link">Ayuda</a>
+                            </li>
+                        </ul>
+                </Drawer>
+            </div>
+            </div>
             <div class="collapse navbar-collapse" id="collapsibleNavId">
                 <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                     <li class="nav-item active">
